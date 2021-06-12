21d1ddb4ab54ed5100692c0cd11f58d62e58eb1c
Fozimat <fozimata@gmail.com>
2021-06-12 20:52:12 +0700

    Implementation Certificate Pinning

--- core/src/main/java/com/fozimat/made/core/di/CoreModule.kt
+++ core/src/main/java/com/fozimat/made/core/di/CoreModule.kt
@@ -9,8 +9,14 @@ import com.fozimat.made.core.data.source.remote.network.ApiService
 import com.fozimat.made.core.domain.repository.IMovieRepository
 import com.fozimat.made.core.utils.AppExecutors
 import com.fozimat.made.core.utils.Constant
+import com.fozimat.made.core.utils.Constant.HOST_NAME
+import com.fozimat.made.core.utils.Constant.PIN_HOSTNAME_1
+import com.fozimat.made.core.utils.Constant.PIN_HOSTNAME_2
+import com.fozimat.made.core.utils.Constant.PIN_HOSTNAME_3
+import com.fozimat.made.core.utils.Constant.PIN_HOSTNAME_4
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -36,10 +42,18 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = HOST_NAME
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, PIN_HOSTNAME_1)
+            .add(hostname, PIN_HOSTNAME_2)
+            .add(hostname, PIN_HOSTNAME_3)
+            .add(hostname, PIN_HOSTNAME_4)
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
--- core/src/main/java/com/fozimat/made/core/utils/Constant.kt
+++ core/src/main/java/com/fozimat/made/core/utils/Constant.kt
@@ -7,4 +7,9 @@ object Constant {
     const val BASE_URL = "https://api.themoviedb.org/"
     const val IMAGE_URL = "https://www.themoviedb.org/t/p/w600_and_h600_bestv2"
     const val DATABASE_NAME = "Movie.db"
+    const val HOST_NAME = "api.themoviedb.org"
+    const val PIN_HOSTNAME_1 = "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0="
+    const val PIN_HOSTNAME_2 = "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA="
+    const val PIN_HOSTNAME_3 = "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI="
+    const val PIN_HOSTNAME_4 = "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I="
 }
