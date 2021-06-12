58f92bf7a344d634321e396a08f491ea37ce072a
Fozimat <fozimata@gmail.com>
2021-06-12 17:56:23 +0700

    Implementation encryption to database

--- core/src/main/java/com/fozimat/made/core/di/CoreModule.kt
+++ core/src/main/java/com/fozimat/made/core/di/CoreModule.kt
@@ -9,6 +9,8 @@ import com.fozimat.made.core.data.source.remote.network.ApiService
 import com.fozimat.made.core.domain.repository.IMovieRepository
 import com.fozimat.made.core.utils.AppExecutors
 import com.fozimat.made.core.utils.Constant
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,11 +22,14 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("fozimat".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDatabase::class.java,
             Constant.DATABASE_NAME
         ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
             .build()
     }
 }
